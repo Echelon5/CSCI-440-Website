aca2988240a9a3f77357de27e14efa649dc41387
Echelon5 <echelon.5@hotmail.com>
2014-11-15 17:46:30 -0700

    New Folder Location

--- Website_combine/script.php
+++ Website_combine/script.php
@@ -1,25 +1,27 @@
 <?php
-$active_region = false; //Active Region booean
-$coronal_hole = false; //Coronal Hole
-$filament = false; //Filament
-$flare = false; //Flare
-$sigmoid = false; //Sigmoid
-$sunspot = false; //Sunspot
-$selected_measurement = $_POST['measurement'];  //Selected measurement
+$active_region = 0; //Active Region booean
+$coronal_hole = 0; //Coronal Hole
+$filament = 0; //Filament
+$flare = 0; //Flare
+$sigmoid = 0; //Sigmoid
+$sunspot = 0; //Sunspot
 
 
-if (isset($_POST['ar'])) 
-{$active_region = true;}
-if (isset($_POST['ch'])) 
-{$coronal_hole = true;}
-if (isset($_POST['fi'])) 
-{$filament = true;}
-if (isset($_POST['fl'])) 
-{$flare = true;}
-if (isset($_POST['sg'])) 
-{$sigmoid = true;}
-if (isset($_POST['ss'])) 
-{$sunspot = true;}
+//$selected_measurement = $_GET['measurement'];  //Selected measurement
 
+
+if (isset($_GET['event_ar'])) {$active_region = 1;}
+if (isset($_GET['event_ch'])) {$coronal_hole = 1;}
+if (isset($_GET['event_fi'])) {$filament = 1;}
+if (isset($_GET['event_fl'])) {$flare = 1;}
+if (isset($_GET['event_sg'])) {$sigmoid = 1;}
+if (isset($_GET['event_ss'])) {$sunspot = 1;}
+
+echo "active_region = $active_region<br>";
+echo "coronal_hole = $coronal_hole<br>";
+echo "filament = $filament<br>";
+echo "flare = $flare<br>";
+echo "sigmoid = $sigmoid<br>";
+echo "sunspot = $sunspot<br>";
 
 ?>
