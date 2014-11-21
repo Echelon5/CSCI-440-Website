ca18b83a523bb85d9a50996f18ed9bd4c9b76315
Echelon5 <echelon.5@hotmail.com>
2014-11-21 09:49:50 -0700

    SortBy StartTime

--- Website_combine/display_data_php.php
+++ Website_combine/display_data_php.php
@@ -222,7 +222,7 @@
 								//echo "final_input = '$final_input'<br><br>";
 								
 								
-								$sql = "SELECT EventID, StartTime, EndTime, EventType FROM Event WHERE StartTime BETWEEN '$final_start_time' AND '$final_end_time' $sql_where";
+								$sql = "SELECT EventID, StartTime, EndTime, EventType FROM Event WHERE StartTime BETWEEN '$final_start_time' AND '$final_end_time' $sql_where SORT BY StartTime";
 								
 								$result = $conn->query($sql);
 								if($result == FALSE){
